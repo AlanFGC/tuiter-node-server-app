5193093bbd29454dd5aa80cb1836e1a8a53b33db
alan <alan_fgc@outlook.com>
2023-04-01 23:45:16 -0400

    Added controller for users hello and tuiter

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import HelloController from './controllers/hello-controller.js'
+import UsersController from "./controllers/users-controller.js";
+import TuitsController
+    from "./controllers/tuits/tuits-controller.js";
+import cors from 'cors'
+const app = express();
+app.use(cors());
+app.use(express.json());
+HelloController(app);
+UsersController(app);
+TuitsController(app);
+app.listen(4000)
